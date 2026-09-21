c4ce1c1092b1b58c693c1c834ea10e421d1c81ca
pm-pmaas <17729447+pm-pmaas@users.noreply.github.com>
2026-09-21 09:11:36 +0200

    Bump version to 1.0.40

--- Config/config.php
+++ Config/config.php
@@ -11,6 +11,6 @@ declare(strict_types=1);
 return [
     'name'        => 'Amazon SES',
     'description' => 'Amazon SES Mailer Plugin for Mautic',
-    'version'     => '1.0.39',
+    'version'     => '1.0.40',
     'author'      => 'e-tailors Europe b.v.',
 ];
